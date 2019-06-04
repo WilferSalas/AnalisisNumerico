ef8558236b6f16c207efc5b3c32259f9b6398485
Manolo <mperezr1@eafit.edu.co>
2019-06-03 22:14:31 -0500

    Cambiando metodos

--- backend/controllers/ecuacionesDeUnaVariable.js
+++ backend/controllers/ecuacionesDeUnaVariable.js
@@ -28,7 +28,7 @@ exports.biseccion = (req, res, next) => {
             });
         }
         
-        let result = gestMetodosCerrados.biseccion(req.body.iter, req.body.tole, req.body.x1, req.body.x2, user.funciones[req.body.numeroFuncion]);
+        let result = gestMetodosCerrados.biseccion(req.body.iter, req.body.tole, req.body.x1, req.body.x2, user.ffuncion);
 
         res.status(200).json({
             message: "Se logro ejecutar biseccion correctamente", 
@@ -61,7 +61,7 @@ exports.reglaFalsa = (req, res, next) => {
         }
         
         
-        let result = gestMetodosCerrados.reglaFalsa(req.body.iter, req.body.tole, req.body.x1, req.body.x2, user.funciones[req.body.numeroFuncion]);
+        let result = gestMetodosCerrados.reglaFalsa(req.body.iter, req.body.tole, req.body.x1, req.body.x2, user.ffuncion);
         console.log(result);
 
         res.status(200).json({
@@ -101,7 +101,7 @@ exports.puntoFijo = (req, res, next) => {
         }
         
         fetchedFunctions = user.funciones;
-        let result = gestMetodosAbiertos.puntoFijo(req.body.iter,req.body.tole, req.body.x1, fetchedFunctions[req.body.numeroFuncion1], fetchedFunctions[req.body.numeroFuncion2]);
+        let result = gestMetodosAbiertos.puntoFijo(req.body.iter,req.body.tole, req.body.x1, user.ffuncion, user.gfuncion);
         console.log(result);
 
         res.status(200).json({
@@ -141,7 +141,7 @@ exports.newton = (req, res, next) => {
         }
         
         fetchedFunctions = user.funciones;
-        let result = gestMetodosAbiertos.newton(req.body.iter,req.body.tole, req.body.x1, fetchedFunctions[req.body.numeroFuncion1], fetchedFunctions[req.body.numeroFuncion2]);
+        let result = gestMetodosAbiertos.newton(req.body.iter,req.body.tole, req.body.x1, user.ffuncion, user.fpfuncion);
         console.log(result);
 
         res.status(200).json({
@@ -175,7 +175,7 @@ exports.secante = (req, res, next) => {
         }
         
         
-        let result = gestMetodosAbiertos.secante(req.body.iter, req.body.tole, req.body.x1, req.body.x2, user.funciones[req.body.numeroFuncion]);
+        let result = gestMetodosAbiertos.secante(req.body.iter, req.body.tole, req.body.x1, req.body.x2, user.ffuncion);
         console.log(result);
 
         res.status(200).json({
@@ -218,9 +218,9 @@ exports.raicesMultiples = (req, res, next) => {
         let result = gestMetodosAbiertos.raicesMultiples(req.body.iter,
             req.body.tole, 
             req.body.x1, 
-            fetchedFunctions[req.body.numeroFuncion1], 
-            fetchedFunctions[req.body.numeroFuncion2],
-            fetchedFunctions[req.body.numeroFuncion3]);
+            user.ffuncion, 
+            user.fpfuncion,
+            user.fppfuncion);
         console.log(result);
 
         res.status(200).json({
